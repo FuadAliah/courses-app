5d613fb22e31b8ef1f1814091b7708fdc67efd02
Fuad Aliah <eye.design17@yahoo.com>
2021-05-04 02:16:09 +0300

    Updates

---
+++ precache-manifest.945c9f063f337ca5187b750e722448a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b61fd8749f9bde264977",
+    "url": "/courses-app/static/css/main.064f7e0b.chunk.css"
+  },
+  {
+    "revision": "b61fd8749f9bde264977",
+    "url": "/courses-app/static/js/main.e0a1227f.chunk.js"
+  },
+  {
+    "revision": "d1168ef6c28a26db55e4",
+    "url": "/courses-app/static/js/runtime~main.1a24c081.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/courses-app/static/js/2.521e897d.chunk.js"
+  },
+  {
+    "revision": "01dbc2d8f55730ed487c86bc17fd70c0",
+    "url": "/courses-app/index.html"
+  }
+];
